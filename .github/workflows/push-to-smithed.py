fa66a845a724e6ca9868acd639fe8d2ad8cb668c
edayot <pro.e.dayot@gmail.com>
2023-07-03 15:37:24 +0200

    chore: smithed release protocol

--- .github/workflows/push-to-smithed.py
+++ .github/workflows/push-to-smithed.py
@@ -6,16 +6,25 @@ import requests
 import json
 import os
 
+try: 
+    SMITHED_UID = os.environ['SMITHED_UID']
+    SMITHED_TOKEN = os.environ['SMITHED_TOKEN']
+    CURRENT_VERSION = os.environ['CURRENT_VERSION']
+    beet = yaml.safe_load(open("beet.yaml"))
+except KeyError:
+    try:
+        with open("credentials.json", "r") as f:
+            creds = json.load(f)
+        SMITHED_UID = creds['SMITHED_UID']
+        SMITHED_TOKEN = creds['SMITHED_TOKEN']
+        CURRENT_VERSION = "0.4.1"
+        beet = yaml.safe_load(open("../../beet.yaml"))
+    except:
+        print("Missing credentials")
+        exit(1)
 
-SMITHED_UID = os.environ['SMITHED_UID']
-SMITHED_TOKEN = os.environ['SMITHED_TOKEN']
-CURRENT_VERSION = os.environ['CURRENT_VERSION']
 
-
-print(os.listdir("."))
-beet = yaml.safe_load(open("beet.yaml"))
-
-beet["id"]
+print(beet)
 
 
 
@@ -42,22 +51,26 @@ except KeyError:
 
 
 pack_version = {
-  "name": beet['name'],
+  "name": CURRENT_VERSION,
   "downloads": {},
   "supports": beet['supports'],
   "dependencies": dep,
 }
 
-if "datapack" in beet:
+if "data_pack" in beet:
     pack_version["downloads"]["datapack"] = download_url.format(ziptype="Datapack")
-if "resourcepack" in beet:
+else:
+    pack_version["downloads"]["datapack"] = ""
+if "resource_pack" in beet:
     pack_version["downloads"]["resourcepack"] = download_url.format(ziptype="Resourcepack")
+else:
+    pack_version["downloads"]["resourcepack"] = ""
 
 
 response = requests.post(
     url=post_url,
     headers={"Content-Type": "application/json"},
-    data=json.dumps(pack_version)
+    data=json.dumps({"data": pack_version})
 )
 
 print(response.status_code)
