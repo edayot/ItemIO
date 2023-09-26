59f8e8970cba1f30663b4e9e74d72bca0a54b26a
edayot <pro.e.dayot@gmail.com>
2023-09-26 22:24:40 +0200

    chore: update makefile

--- beet_watch.sh
+++
@@ -1,4 +0,0 @@
-beet l --minecraft "/home/erwan/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/Snapshot -/.minecraft/" 
-beet l --data-pack "/home/erwan/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/Snapshot -/.minecraft//saves/New World/datapacks" 
-beet l --resource-pack "/home/erwan/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/Snapshot -/.minecraft//resourcepacks"
-beet --project beet-dev.yaml w
